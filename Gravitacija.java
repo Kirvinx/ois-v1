0db97448abdc62f73531085cc4d8f1d82c746a69
Kirvinx <rf5885@student.uni-lj.si>
2022-02-25 20:46:48 +0100

    Poskus

--- Gravitacija.java
+++ Gravitacija.java
@@ -6,19 +6,8 @@ import java.util.Scanner;
 
 public class Gravitacija {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		Scanner sc = new Scanner(System.in);
-		double v = sc.nextDouble();
-		double a = izracunajGravitacijo(v);
-		System.out.println(a);
-		
-	}
-=======
-	
 	Scanner sc = new Scanner(System.in);
 	double visina = sc.hasNextDouble();
-	
->>>>>>> ee5b8b9462b65a36a729873a8a5c9c544dc78e8f
 	
 	
 	//izpis gravitacijskega pospeška za nadmorsko višino iz ukazne vrstice, metoda gravitacija mora vrnit double!
