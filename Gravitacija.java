fe2d00076e5cbfe14aa4148c424a5dda8bcbf659
ErazemStanonik <es0402@student.uni-lj.si>
2022-02-25 20:58:31 +0100

    Dodaj izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -8,7 +8,7 @@ public class Gravitacija {
 	public static void main(String[] args) {
 	
 	Scanner sc = new Scanner(System.in);
-	double visina = sc.hasNextDouble();
+	double visina = sc.nextDouble();
 	
 	
 	
@@ -16,6 +16,14 @@ public class Gravitacija {
 	System.out.printf("Gravitacijski pospešek: %f%n", gravitacija(visina));
 
 
+	}
+    // dodau Erazem. Upam da bo prou...
+	public static double gravitacija(double visina) {
+	    final double GRAVITACIJSKA_KONSTANTA = 6.674 * Math.pow(10, -11);
+	    final double MASA_ZEMLJE = 5.972 * Math.pow(10, 24);
+	    final double POLMER_ZEMLJE = 6.371 * Math.pow(10, 6);
+
+	    return ((GRAVITACIJSKA_KONSTANTA * MASA_ZEMLJE) / Math.pow((POLMER_ZEMLJE + visina), 2));
 	}
 	
 	
